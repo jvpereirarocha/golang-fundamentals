6219ab8925aa0c1d1d5f9ec63b50da0e8c7d3a13
Joao Victor <jvrochadev@gmail.com>
2025-04-21 21:15:09 -0300

    feat: adding new code to values and variables in go files

--- goExample/values/values.go
+++ goExample/values/values.go
@@ -11,4 +11,12 @@ func main() {
 	fmt.Println(true && false)
 	fmt.Println(true || false)
 	fmt.Println(!true)
+
+	fmt.Println("hello " + "world")
+	fmt.Println("3+2 =", 3+2)
+	fmt.Println("10.0/5.2 = ", 10.0/5.2)
+
+	fmt.Println(true && true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
 }
--- goExample/variables/variables.go
+++ goExample/variables/variables.go
@@ -15,6 +15,13 @@ func main() {
 	var e int
 	fmt.Println(e)
 
+	/* The syntax below is available only inside functions */
 	f := "apple"
 	fmt.Println(f)
+
+	var t string = "allow"
+	fmt.Println("T value => ", t)
+
+	z := "zeta"
+	fmt.Printf("The letter is %s\n", z)
 }
